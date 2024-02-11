1ecf6198cccae6d2409bbdceb12ecb99602e70e3
Himanshu Tiwari <himanshutiwari@Himanshus-MacBook-Air.local>
2024-02-12 00:51:18 +0800

    Added CSS

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,21 +1,37 @@
 import React from "react";
 
 export default function MainContent(){
-  return(
-    <>
-    <h1>YOUR FEET DESERVE THE BEST</h1>
-    <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
-    <button>SHOP NOW</button>
-    <button>Category</button>
-    <p>Also Available On</p>
-    <img src="../../public/flipkart.png"/>
-    <img src="../../public/amazon.png"/>
-    <img src="../public/brand_logo.png"/>
 
-    </>
-   
-  )
-}
+    return (
+      <main className="hero container">
+        <div className="hero-content">
+          <h1>YOUR FEET DESERVE THE BEST</h1>
+          <p>
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </p>
+  
+          <div className="hero-btn">
+            <button>Shop Now </button>
+            <button className="secondary-btn">Category</button>
+          </div>
+  
+          <div className="shopping">
+            <p>Also Available On</p>
+  
+            <div className="brand-icons">
+              <img src="/amazon.png" alt="amazon-logo" />
+              <img src="/flipkart.png" alt="flipkart-logo" />
+            </div>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src="/shoe_image.png" alt="hero-image" />
+        </div>
+      </main>
+    );
+  }
 
 
 
